c0365148e90eb39afca313c742942035fd8253cc
Padma Sai <padmasaigorrela@gmail.com>
2024-09-18 00:17:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+    const backToTopBtn = document.getElementById('backToTopBtn');
+
+    window.onscroll = function() {
+        scrollFunction();
+    };
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            backToTopBtn.style.display = "block";
+        } else {
+            backToTopBtn.style.display = "none";
+        }
+    }
+    backToTopBtn.addEventListener('click', function () {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    });
